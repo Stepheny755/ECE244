0631f3e67b5c5eea275212cfb3d62586d16be69b
Stephen <stepheny755@gmail.com>
2020-09-27 11:15:33 -0400

    implemented basic function implementations from GameState.h

--- Labs/Lab_2/GameState.cpp
+++ Labs/Lab_2/GameState.cpp
@@ -16,4 +16,64 @@
 
 // ECE244 Student: add you code below
 
+GameState::GameState(){
+  selectedRow = 0;
+  selectedColumn = 0;
+  moveValid = true;
+  gameOver = false;
+  winCode = 0;
+  turn = true;
+  for(int i = 0;i < boardSize;i++){
+    for(int j = 0;j < boardSize;j++){
+      gameBoard[i][j] = Empty;
+    }
+  }
+}
 
+int GameState::get_selectedRow(){
+
+}
+
+int GameState::get_selectedColumn(){
+  
+}
+
+bool GameState::get_moveValid(){
+  return moveValid;
+}
+
+void GameState::set_moveValid(bool value){
+  moveValid = value;
+}
+
+bool GameState::get_gameOver(){
+  return gameOver;
+}
+
+void GameState::set_gameOver(bool value){
+  gameOver = value;
+}
+
+int GameState::get_winCode(){
+  return winCode;
+}
+
+void GameState::set_winCode(int value){
+  winCode = value;
+}
+
+bool GameState::get_turn(){
+  return turn;
+}
+
+void GameState::set_turn(bool val){
+  turn = val;
+}
+
+int GameState::get_gameBoard(int row,int col){
+  return 0;
+}
+
+void GameState::set_gameBoard(int row,int col,int value){
+
+}
